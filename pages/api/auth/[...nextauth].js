aaa9e6c9d413af207880d6effaf775ad397f2198
andyG-hub <andydraganic@gmail.com>
2026-02-01 20:26:47 +0100

    Update [...nextauth].js

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,20 +1,21 @@
 import NextAuth from "next-auth";
 import TwitterProvider from "next-auth/providers/twitter";
 
-export default NextAuth({
+export const authOptions = {
   providers: [
     TwitterProvider({
       clientId: process.env.TWITTER_CLIENT_ID,
       clientSecret: process.env.TWITTER_CLIENT_SECRET,
-      version: "2.0", // This is very important for X OAuth 2.0
+      version: "2.0",
     }),
   ],
   callbacks: {
     async session({ session, token }) {
-      // This part ensures the bot knows the user's handle (@name)
-      session.user.username = token.screen_name || token.name; 
+      session.user.username = token.screen_name || token.name;
       return session;
     },
   },
   secret: process.env.NEXTAUTH_SECRET,
-});
+};
+
+export default NextAuth(authOptions);
